54bf3b9d1faf37c23faf87e6bb55543d0d41dbe6
Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
2012-02-24 16:03:47 -0200

    Plugin para remoção de grupos.

--- admin/aura/inc/groups.php
+++ admin/aura/inc/groups.php
@@ -23,6 +23,30 @@ class Groups {
 	public static function getDevicesById($theGroupId) {
 	
 	}
+	
+	public static function getByClue($theClue) {
+		$aRet	 = null;
+		$aWhere  = is_numeric($theClue) ? "id = ".$theClue : "name like '".addslashes($theClue)."'";
+		$aResult = Db::execute("SELECT * FROM ".Db::TABLE_GROUPS." WHERE ". $aWhere);
+		
+		if(Db::numRows($aResult) == 1) {
+			$aRet = Db::fetchAssoc($aResult);
+		}
+		
+		return $aRet;
+	}
+	
+	/**
+	 * Remove um grupo do banco de dados, com base em uma pista. Se a pista for
+	 * um número, ela será tratada como o id; se for uma string, será considerada o nome.
+	 * 
+	 * @param mixed $theSlug um inteiro representando o id do grupo, ou uma string representando o nome do grupo.
+	 * @return boolean true se conseguiu apagar algo, ou false caso contrário.
+	 */
+	public static function removeByClue($theClue) {
+		$aWhere = is_numeric($theClue) ? "id = ".$theClue : "name like '".addslashes($theClue)."'"; 
+		return Db::execute("DELETE FROM ".Db::TABLE_GROUPS." WHERE ". $aWhere);
+	} 
 }
 
 ?>
