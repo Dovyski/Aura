ba5d117c698ac911a2bfa80da5ae38c31f92ee62
Fernando Bevilacqua <fernando.bevilacqua@uffs.edu.br>
2012-02-28 17:24:55 -0300

    API para gerenciamento de pings.

--- admin/aura/globals.php
+++ admin/aura/globals.php
@@ -6,6 +6,7 @@ require_once dirname(__FILE__).'/inc/utils.php';
 require_once dirname(__FILE__).'/inc/db.php';
 require_once dirname(__FILE__).'/inc/commands.php';
 require_once dirname(__FILE__).'/inc/tasks.php';
+require_once dirname(__FILE__).'/inc/pings.php';
 require_once dirname(__FILE__).'/inc/devices.php';
 require_once dirname(__FILE__).'/inc/groups.php';
 require_once dirname(__FILE__).'/inc/interpreter.php';
---
+++ admin/aura/inc/pings.php
@@ -0,0 +1,64 @@
+<?php 
+
+/**
+ * Gerenciamento e manipulação de pings. Os pings são mensagens enviadas
+ * pelos clientes Aura para informar o seu status atual, como acesso à internet,
+ * espaço em disco, usuários logados, etc.
+ */
+
+namespace Aura;
+
+class Pings {
+	/**
+	 * Insere um ping novo no banco de dados.
+	 * 
+	 * @param int $theDeviceId id do dispositivo que gerou o ping.
+	 * @param array $theInfos array assossiativo com as informações a serem inseridas.
+	 * @throws \Exception
+	 */
+	public static function add($theDeviceId, $theInfos) {
+		$theDeviceId = (int)$theDeviceId;
+		
+		if(empty($theInfos['data'])) {
+			throw new \Exception('Ping sem qualquer informação.');
+		}
+		
+		if(Devices::getByClue($theDeviceId) == null) {
+			throw new \Exception('O dispositivo informado não existe.');
+		}
+		
+		$aInfo 				= Utils::prepareForSql($theInfos);
+		$aInfo['fk_device'] = $theDeviceId; 
+		
+		Db::execute("INSERT INTO ".Db::TABLE_PINGS." (`".implode("`,`", array_keys($aInfo))."`) VALUES (".implode(',', $aInfo).")");
+	}
+
+	/** 
+	 * Obtem informações de pings de um grupo de dispositivos.
+	 * 
+	 * @param array $theIds array com os ids dos dispositivos a serem buscados.
+	 * @param int $theSinceUnixtime timestamp a partir do qual os logs serão buscados.
+	 * @return array array assossiativo com informações dos pings, no formato [id_device] => array(pings).
+	 */
+	public static function findByDevices($theIds, $theSinceUnixtime) {
+		$aRet	 			= array();
+		$aIds 				= Utils::prepareForSql($theIds);
+		$theSinceUnixtime 	= (int)$theSinceUnixtime;
+		
+		$aResult 			= Db::execute("SELECT * FROM ".Db::TABLE_PINGS." WHERE fk_device IN (".implode(',', $aIds).") AND time >= " . $theSinceUnixtime);
+		
+		if(Db::numRows($aResult) > 0) {
+			while($aRow = Db::fetchAssoc($aResult)) {
+				$aRet[$aRow['fk_device']][] = $aRow;
+			}
+		}
+		
+		return $aRet;
+	}
+	
+	public static function remove($theOlderThanTimestamp) {
+		$theOlderThanTimestamp = (int)$theOlderThanTimestamp;
+		return Db::execute("DELETE FROM ".Db::TABLE_PINGS." WHERE time <= " . $theOlderThanTimestamp);
+	}
+}
+?>
