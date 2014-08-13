869280e321d779ecb870761f8c90100f19f92ecc
Fernando Bevilacqua <dovyski@gmail.com>
2014-08-13 14:02:47 -0300

    Fix problem with unattended format on windows.

--- admin/aura/inc/sentenses/FormatDevices.php
+++ admin/aura/inc/sentenses/FormatDevices.php
@@ -8,7 +8,7 @@ function formatDevices($theGroupName) {
 
 	if($aHowManyDevices > 0) {
 		$aCommand = array(
-			'win' 	=> 'sed "s/.*set default=\"[0-9]\".*/   set default=\"3\"/" o:\boot\grub\grub.cfg > o:\boot\grub\grub.cfg.tmp & cat o:\boot\grub\grub.cfg.tmp > o:\boot\grub\grub.cfg & shutdown -r -t 60 & msg * "O computador vai ser formatado em 1 minuto. Salve tudo aberto agora!"',
+			'win' 	=> 'sed "s/.*set default=\"[0-9]\".*/   set default=\"3\"/" o:\boot\grub\grub.cfg > o:\boot\grub\grub.cfg.tmp & attrib -r o:\boot\grub\grub.cfg & cat o:\boot\grub\grub.cfg.tmp > o:\boot\grub\grub.cfg & shutdown -r -t 60 & msg * "O computador vai ser formatado em 1 minuto. Salve tudo aberto agora!"',
 			'linux' => 'sed \'s/.*set default=\"[0-9]\".*/   set default=\"3\"/\' /boot/grub/grub.cfg > /boot/grub/grub.cfg.tmp ; cat /boot/grub/grub.cfg.tmp > /boot/grub/grub.cfg ; shutdown -r +1 >/dev/null 2>/dev/null &',
 			'mac' 	=> '',
 		);
